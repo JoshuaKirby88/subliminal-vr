e2845d9d98268698a5e677c53b6d077185fcf18f
Joshua Kirby <jojokirby88@gmail.com>
2026-02-05 21:02:38 +0000

    Bump alpha release version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
     // HorizonOS is Android 14 (API level 34)
     //noinspection OldTargetApi,ExpiredTargetSdkVersion
     targetSdk = 34
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "1.1.0-alpha01"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
